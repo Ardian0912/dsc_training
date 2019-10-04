3c93e07e4a793e0ebdea585ae9f4920c9a03fb0e
Ardian0912 <muhammadnurwidya.2019@student.uny.ac.id>
2019-10-04 17:22:23 +0700

    penjumlahan

--- custom.js
+++ custom.js
@@ -5,3 +5,13 @@ function gantikalimat(){
     $('#banner').text('Hallo, '+nama+'!');
 
 }
+
+function jumlah(){
+    // alert("oke");
+    var angka1 = parseInt($('#angka1').val());
+    var angka2 = parseInt($('#angka2').val());
+
+    var hasil = angka1 + angka2;
+
+    $('#hasil').val(hasil);
+}
